ab4c9c87c72a113b7372dd6c0c472d846645af97
imad9595 <imad.mawass@itsvolta.it>
2026-06-18 12:03:32 +0200

    nuovo aggiorVOF

--- triage/MainTriage.java
+++ triage/MainTriage.java
@@ -168,25 +168,26 @@ public class MainTriage {
                 break;
 
             case "2":
-<<<<<<< HEAD
                 // Controllo e inserimento parteCorpo (ortopedica)
-                boolean cond5 = false;
-                while (!cond5) {
-                    System.out.println("Inserire la zona del corpo interessata:");
-                    parteCorpo = sc.nextLine().trim();
-                    parteCorpo = parteCorpo.substring(0, 1).toUpperCase() + parteCorpo.substring(1);
-
-                    if (parteCorpo.length() < 50 && parteCorpo.length() > 0) {
-                        cond5 = true;
-                    } else {
-                        System.err
-                                .println("⚠️ Errore di inserimento, Errore di inserimento, lunghezza testo non valida");
-                    }
-                }
-
+                /*
+                 * boolean cond5 = false;
+                 * while (!cond5) {
+                 * System.out.println("Inserire la zona del corpo interessata:");
+                 * parteCorpo = sc.nextLine().trim();
+                 * parteCorpo = parteCorpo.substring(0, 1).toUpperCase() +
+                 * parteCorpo.substring(1);
+                 * 
+                 * if (parteCorpo.length() < 50 && parteCorpo.length() > 0) {
+                 * cond5 = true;
+                 * } else {
+                 * System.err
+                 * .println("⚠️ Errore di inserimento, Errore di inserimento, lunghezza testo non valida"
+                 * );
+                 * }
+                 * }
+                 */
                 // Creazione visita specifica
                 visita = new VisitaOrtopedica(parteCorpo, dat, med, dia);
-=======
                 VisitaOrtopedicaFactory visitaOrtoFactory = new VisitaOrtopedicaFactory();
                 visita = visitaOrtoFactory.creareVisita();
                 /*
@@ -210,7 +211,6 @@ public class MainTriage {
                  * // Creazione visita specifica
                  * visita = new VisitaOrtopedica(parteCorpo, dat, med, dia);
                  */
->>>>>>> ec966050b8686a11ba2f3710672f145faf0406c4
                 break;
 
             case "3":
